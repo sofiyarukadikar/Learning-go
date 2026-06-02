5ecead55d2e9f509f6661d241a95a4d7f07a6d48
sofiyarukadikar <sofiyarukadikar@gmail.com>
2026-06-02 19:52:50 +0530

    add tut 2

---
+++ 02Variables/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+func main() {
+	//declare and initialize
+	var username string = "sofiya"
+	fmt.Println(username)
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+
+	//declare
+	var num int
+	fmt.Println(num)
+
+	//no var style
+	demo := "a string"
+	fmt.Println(demo)
+}
