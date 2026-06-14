62c98aa5c8612dc08226daee9603d7276f123b56
sofiyarukadikar <sofiyarukadikar@gmail.com>
2026-06-14 19:57:59 +0530

    add arrays

---
+++ 07Arrays/go.mod
@@ -0,0 +1,3 @@
+module myarrays
+
+go 1.26.3
---
+++ 07Arrays/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var array [5]int
+	array[0] = 1
+	array[1] = 2
+	array[2] = 3
+	array[3] = 4
+	array[4] = 5
+	fmt.Println(array)
+
+	var fruitList = [3]string{"Apple", "Banana", "Cherry"}
+	fmt.Println(fruitList)
+
+}
