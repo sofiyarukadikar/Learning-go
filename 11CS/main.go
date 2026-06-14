75a829058c73ecb611a04fe8124c06fdf3fa32d3
sofiyarukadikar <sofiyarukadikar@gmail.com>
2026-06-14 20:40:51 +0530

    add cs

---
+++ 11CS/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main(){
+
+logincount := 23
+var result string
+
+if logincount <10{
+	result = "regular user"
+}else{
+	result ="Niceeee"
+}
+
+fmt.Println(result)
+ //using if lese with initializations
+if num :=3 ;num<10{
+	fmt.Println("Num is less than 10")
+}else{
+		fmt.Println("Num is greater than 10")
+
+}
+
+
+}
